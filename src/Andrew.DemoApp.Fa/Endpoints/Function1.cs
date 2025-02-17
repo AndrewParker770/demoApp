3e5032623d12a46fa0b12d878bbffbdac53e97c5
AndrewParker770 <andrewparker767@yahoo.co.uk>
2025-02-17 21:02:24 +0000

    A1 - Creating framework of application

---
+++ src/Andrew.DemoApp.Fa/Endpoints/Function1.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace Andrew.DemoApp.Fa
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(
+                AuthorizationLevel.Function,
+                "get",
+                "post",
+                Route = null)]
+            HttpRequestMessage req)
+        {
+            return null;
+        }
+    }
+}
